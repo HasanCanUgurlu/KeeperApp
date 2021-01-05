5d118489daba14a3c90fcb916d57e9dbf076cd4a
HasanCanUgurlu <63429082+HasanCanUgurlu@users.noreply.github.com>
2021-01-05 11:27:41 -0700

    initial commit

---
+++ build/precache-manifest.c6d76ecd5647c8337d66d8de0dfc61a2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3badeb5eba5aee4eba6dbb00a4cefde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac1cf2473235757e9029",
+    "url": "/static/js/2.bd058660.chunk.js"
+  },
+  {
+    "revision": "ecd517e179ec8b554680",
+    "url": "/static/js/main.ab8f2967.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
